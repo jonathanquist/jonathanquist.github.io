7fce43e082301f09763d63b805f49ff7d51c88f1
Jonathan <jonathan.quist@outlook.com>
2024-05-24 19:09:28 +0200

    feat: Vercel redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-import { useRouter } from "next/router";
 
 export default function Page() {
   const router = useRouter();
 
   useEffect(() => {
-    router.push("https://portfolio-alpha-rosy-36.vercel.app/about");
+    if (typeof window !== "undefined") {
+      // check if code is running on client side
+      router.push("https://portfolio-alpha-rosy-36.vercel.app/about");
+    }
   }, [router]);
 
   return (
